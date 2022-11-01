61d3d0fcef8c316191cd165d58decfdf8f3eba73
Dmitriy-1 <dimalapyghin@yandex.ru>
2022-11-01 16:27:46 +0300

    Add autorization with passport

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        //api защита аутентификации
+        'api' => [
+            'driver' => 'passport',
+            'provider' => 'users',
+        ],
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Use \App\Http\Controllers\UserController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
